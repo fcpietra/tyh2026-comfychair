6ba72c802c90f026f36ba4a6a5fea764301650a1
fabriciopasinato <65682723+fabriciopasinato@users.noreply.github.com>
2026-05-17 22:23:56 -0300

    funciones seleccion de articulos

--- src/Session.js
+++ src/Session.js
@@ -1,29 +1,32 @@
-const {Bid, Interests} = require("./Bid");
+const { Bid, Interests } = require("./Bid");
 
 const STAGES = {
     receiving: "Receiving",
     bidding: "Bidding",
-    reviewing: "Reviewing"
+    reviewing: "Reviewing",
+    selection: "Selection"
 }
 
-class Session{
-    constructor(){
+class Session {
+    constructor() {
         this._name = "";
-        this._programCommittee=[];
-        this._papers=[];
-        this._bids=[];
-        this._stage=STAGES.receiving;
+        this._programCommittee = [];
+        this._papers = [];
+        this._bids = [];
+        this._stage = STAGES.receiving;
+        this._acceptancePercentage = 0;
+        this._acceptedPapers = [];
     }
-    name(){
+    name() {
         return this._name;
     };
-    programCommittee(){
+    programCommittee() {
         return this._programCommittee;
     };
-    reviewers(){
+    reviewers() {
         return this._programCommittee;
     };
-    addReviewer(user){
+    addReviewer(user) {
         this._programCommittee.push(user);
     }
     canSubmit(paper){
@@ -32,7 +35,7 @@ class Session{
         else
             return false;
     }
-    submit(paper){
+    submit(paper) {
         if (!this.canSubmit(paper)) throw new Error("Cannot submit invalid paper");
 
         if (this.stage() == STAGES.receiving )
@@ -40,16 +43,16 @@ class Session{
         else
             throw new Error("Cannot submit papers at this stage");
     }
-    papers(){
+    papers() {
         return this._papers;
     }
-    bids(){
+    bids() {
         return this._bids;
     }
-    stage(){
+    stage() {
         return this._stage;
     }
-    setStage(stage){
+    setStage(stage) {
         this._stage = stage;
     }
     closeSubmissions(){
@@ -61,21 +64,43 @@ class Session{
                 let existing =  this.bidFor(paper, reviewer);
                 existing.setInterest(interest);
             }
-            else{
+            else {
                 let bid = new Bid(paper, reviewer, interest);
                 this._bids.push(bid);
             }
         else
             throw new Error("Cannot enter bids from the current stage.");
     }
-    bidExistsFor(paper, reviewer){
-        return typeof(this.bidFor(paper, reviewer)) != "undefined";
+    bidExistsFor(paper, reviewer) {
+        return typeof (this.bidFor(paper, reviewer)) != "undefined";
     }
-    bidFor(paper, reviewer){
-        return this._bids.find( (suspect) => (suspect.paper() == paper) && (suspect.reviewer()==reviewer) );
+    bidFor(paper, reviewer) {
+        return this._bids.find((suspect) => (suspect.paper() == paper) && (suspect.reviewer() == reviewer));
     }
-    interestFor(paper, reviewer){
+    interestFor(paper, reviewer) {
         return this.bidFor(paper, reviewer).interest();
+    }
+    setAcceptancePercentage(percentage) {
+        if (percentage < 0 || percentage > 100) throw new Error("Percentage must be between 0 and 100");
+        this._acceptancePercentage = percentage;
+    }
+    acceptancePercentage() {
+        return this._acceptancePercentage;
+    }
+    selectArticles() {
+        if (this.stage() !== STAGES.selection)
+            throw new Error("No se pueden seleccionar artículos en esta etapa");
+
+        let sortedPapers = [...this._papers].sort(function (a, b) {
+            return b.score() - a.score();
+        });
+
+        let maxAccepted = Math.floor(this._acceptancePercentage / 100 * this._papers.length);
+        this._acceptedPapers = sortedPapers.slice(0, maxAccepted);
+        return this._acceptedPapers;
+    }
+    acceptedPapers() {
+        return this._acceptedPapers;
     }
 }
 
